36ceab02d10d1e993db9f69851ef0d4999e33cce
teuaraujoo <empresarial.mateus25@gmail.com>
2026-02-09 10:59:08 -0300

    ajuste animações - 3

--- assets/script/animations/loader.js
+++ assets/script/animations/loader.js
@@ -3,6 +3,12 @@ export function loader() {
 
     let preloader = document.querySelector(".preloader");
 
+    document.body.style.overflow = 'hidden';
+    
+    setTimeout(() => {
+        document.body.style.overflow = 'visible';
+    }, 1480)
+
     setTimeout(() => {
         preloader.classList.add("loaded");
     }, 1800);
