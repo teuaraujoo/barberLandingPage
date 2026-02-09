0cf217e61b222896854222f508971dc63e1f8ce3
teuaraujoo <empresarial.mateus25@gmail.com>
2026-02-09 11:02:48 -0300

    ajuste overflow

--- assets/script/animations/loader.js
+++ assets/script/animations/loader.js
@@ -6,7 +6,7 @@ export function loader() {
     document.body.style.overflow = 'hidden';
     
     setTimeout(() => {
-        document.body.style.overflowY = 'visible';
+        document.body.style.overflow = 'visible';
     }, 1480)
 
     setTimeout(() => {
